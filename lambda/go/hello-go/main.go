3456639ccce72a81abd10933dd849bd35432f23b
t2run <42409759+t2run@users.noreply.github.com>
2020-08-11 23:13:04 +0530

    Update main.go
    Initial Commit

--- lambda/go/hello-go/main.go
+++ lambda/go/hello-go/main.go
@@ -1,1 +1,23 @@
 
+package main
+
+import (
+	"fmt"
+
+	"github.com/aws/aws-lambda-go/lambda"
+)
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler() {
+	fmt.Println("Hello Lambda from Go")
+}
+
+//To build for linux
+// $env:GOOS = "linux"
+// $env:CGO_ENABLED = "0"
+// $env:GOARCH = "amd64"
+// go build -o main main.go
+// ~\Go\Bin\build-lambda-zip.exe -output main.zip main
