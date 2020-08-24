8c4e79503a49a379dd31b691d57e505f0303f31a
t2run <42409759+t2run@users.noreply.github.com>
2020-08-24 13:06:22 +0530

    Create main.go

---
+++ lambda/go/go-auth/main.go
@@ -0,0 +1,147 @@
+package main
+
+import (
+	"context"
+	"crypto/hmac"
+	"crypto/sha256"
+	"encoding/hex"
+	"errors"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/aws/aws-sdk-go/aws"
+	"github.com/aws/aws-sdk-go/aws/awserr"
+	"github.com/aws/aws-sdk-go/aws/session"
+	"github.com/aws/aws-sdk-go/service/secretsmanager"
+)
+
+type myReturn struct {
+	Response string `json:"response"`
+}
+
+var (
+	// ErrNameNotProvided is thrown when a name is not provided
+	ErrNameNotProvided = errors.New("no name was provided in the HTTP body")
+)
+
+func main() {
+	lambda.Start(handler)
+}
+
+func handler(ctx context.Context, request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+	log.Printf("Processing request data for request %s.\n", request.RequestContext.RequestID)
+	log.Printf("Body size = %d.\n", len(request.Body))
+
+	log.Println("Headers:")
+	ubeSign := "notvalid"
+	for key, value := range request.Headers {
+
+		if key == "signat" {
+			ubeSign = value
+		}
+		log.Printf("    %s: %s\n", key, value)
+	}
+
+	//return events.APIGatewayProxyResponse{code, headers, string(response), false}, nil
+
+	//reading a secret
+
+	//secret := "mysecret"
+	secret := getSecret()
+	fmt.Println(" SEcret obtained is : " + secret)
+	data := "data"
+	fmt.Printf("Secret: %s Data: %s\n", secret, data)
+
+	// Create a new HMAC by defining the hash type and the key (as byte array)
+	h := hmac.New(sha256.New, []byte(secret))
+
+	// Write Data to it
+	h.Write([]byte(data))
+
+	// Get result and encode as hexadecimal string
+	sha := hex.EncodeToString(h.Sum(nil))
+
+	fmt.Println("Result: " + sha)
+
+	respString := "Hello " + request.Body
+	log.Printf("Returning string: %v", respString)
+
+	statusCode := 200
+	if sha == ubeSign {
+		statusCode = 200
+	} else {
+		statusCode = 403
+	}
+	return events.APIGatewayProxyResponse{Body: respString, StatusCode: statusCode, IsBase64Encoded: false}, nil
+
+}
+
+func getSecret() (secretString string) {
+	secretName := "test/secret/lambda"
+	region := "us-east-2"
+
+	//Create a Secrets Manager client
+	svc := secretsmanager.New(session.New(),
+		aws.NewConfig().WithRegion(region))
+	input := &secretsmanager.GetSecretValueInput{
+		SecretId:     aws.String(secretName),
+		VersionStage: aws.String("AWSCURRENT"), // VersionStage defaults to AWSCURRENT if unspecified
+	}
+
+	// In this sample we only handle the specific exceptions for the 'GetSecretValue' API.
+	// See https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
+
+	result, err := svc.GetSecretValue(input)
+	if err != nil {
+		if aerr, ok := err.(awserr.Error); ok {
+			switch aerr.Code() {
+			case secretsmanager.ErrCodeDecryptionFailure:
+				// Secrets Manager can't decrypt the protected secret text using the provided KMS key.
+				fmt.Println(secretsmanager.ErrCodeDecryptionFailure, aerr.Error())
+
+			case secretsmanager.ErrCodeInternalServiceError:
+				// An error occurred on the server side.
+				fmt.Println(secretsmanager.ErrCodeInternalServiceError, aerr.Error())
+
+			case secretsmanager.ErrCodeInvalidParameterException:
+				// You provided an invalid value for a parameter.
+				fmt.Println(secretsmanager.ErrCodeInvalidParameterException, aerr.Error())
+
+			case secretsmanager.ErrCodeInvalidRequestException:
+				// You provided a parameter value that is not valid for the current state of the resource.
+				fmt.Println(secretsmanager.ErrCodeInvalidRequestException, aerr.Error())
+
+			case secretsmanager.ErrCodeResourceNotFoundException:
+				// We can't find the resource that you asked for.
+				fmt.Println(secretsmanager.ErrCodeResourceNotFoundException, aerr.Error())
+			}
+		} else {
+			// Print the error, cast err to awserr.Error to get the Code and
+			// Message from an error.
+			fmt.Println(err.Error())
+		}
+		return
+	}
+
+	// Decrypts secret using the associated KMS CMK.
+	// Depending on whether the secret is a string or binary, one of these fields will be populated.
+	//var decodedBinarySecret string
+	if result.SecretString != nil {
+		secretString = *result.SecretString
+	} else {
+		//decodedBinarySecretBytes := make([]byte, base64.StdEncoding.DecodedLen(len(result.SecretBinary)))
+		//len, err := base64.StdEncoding.Decode(decodedBinarySecretBytes, result.SecretBinary)
+		if err != nil {
+			fmt.Println("Base64 Decode Error:", err)
+			return
+		}
+		//decodedBinarySecret = string(decodedBinarySecretBytes[:len])
+	}
+
+	return secretString
+
+	// Your code goes here.
+}
